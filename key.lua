974ec10f58b26a9d89e6ee25f1fabfeaae1a55bd
srpitre1 <artemkruykov@yandex.ru>
2024-05-26 14:33:54 +0300

    Update key.lua

--- key.lua
+++ key.lua
@@ -13,7 +13,7 @@ function Makescr1ptHub()
 end
 Tab:AddTextbox({
 	Name = "Key",
-	Default = "default box input",
+	Default = "",
 	TextDisappear = true,
 	Callback = function(Value)
      _G.KeyInput = Value
